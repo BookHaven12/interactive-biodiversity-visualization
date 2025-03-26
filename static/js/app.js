ecc6a56961536b37bcde4a93a5a1c16e93bdd709
BookHaven12 <iyermegan@gmail.com>
2025-03-26 11:38:00 -0700

    Code finished

--- static/js/app.js
+++ static/js/app.js
@@ -40,7 +40,7 @@ function buildCharts(sample) {
     // Build a Bubble Chart
     let bubbleTrace = {
       x: otu_ids,
-      y: samples,
+      y: sample_values,
       text: otu_labels,
       type: "scatter",
       mode: "markers",
@@ -60,16 +60,16 @@ function buildCharts(sample) {
     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
- 
+    let yticks = otu_ids.map(d => `OTU ${d} `)
 
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
-    let sortedData = 
-    
-
-    
+  
     let barTrace = {
-      x:
+      x: sample_values.slice(0,10).reverse(),
+      y: yticks.slice(0,10).reverse(),
+      type: "bar",
+      orientation: "h"
     }
 
     let barData = [barTrace]
@@ -88,28 +88,32 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
     console.log(data);
     // Get the names field
-
+    let names = data.names;
 
     // Use d3 to select the dropdown with id of `#selDataset`
-
+    let dropdown = d3.select(`#selDataset`);
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
-
+    for (let i=0; i < names.length; i++) {
+      dropdown.append("option").text(names[i]).property("value",names[i]);
+    }
 
     // Get the first sample from the list
-
+    let firstSample = names[0];
 
     // Build charts and metadata panel with the first sample
-
+    buildMetadata(firstSample);
+    buildCharts(firstSample);
   });
 }
 
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
-
+  buildMetadata(newSample);
+  buildCharts(newSample);
 }
 
 // Initialize the dashboard
